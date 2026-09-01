f4712ceb0adc4f8157a99e8a24d4fd7c4a957049
eather009 <eather.ahmed@gmail.com>
2026-09-01 22:40:04 +0900

    Pop from stash

---
+++ feature.py
@@ -0,0 +1,12 @@
+import random
+
+print ("Hello")
+
+name = input ("What is your name?")
+
+print (f"Welcome to python, {name}!");
+
+
+secret_number = random.randint(1,20)
+
+print ("Your age is {secret_number}")
